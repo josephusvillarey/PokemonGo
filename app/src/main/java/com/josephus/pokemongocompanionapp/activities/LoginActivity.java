17dd62cfecc801f6a25c5255151627aa70511578
Josephus Villarey <josephusv@gmail.com>
2016-09-30 07:42:55 +0800

    login flow with token persisted

--- app/src/main/java/com/josephus/pokemongocompanionapp/activities/LoginActivity.java
+++ app/src/main/java/com/josephus/pokemongocompanionapp/activities/LoginActivity.java
@@ -1,10 +1,15 @@
 package com.josephus.pokemongocompanionapp.activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.josephus.pokemongocompanionapp.PokemonGo;
@@ -12,6 +17,7 @@ import com.josephus.pokemongocompanionapp.R;
 import com.pokegoapi.auth.GoogleUserCredentialProvider;
 import com.pokegoapi.exceptions.LoginFailedException;
 import com.pokegoapi.exceptions.RemoteServerException;
+import java.util.concurrent.ExecutionException;
 import okhttp3.OkHttpClient;
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,6 +26,9 @@ public class LoginActivity extends AppCompatActivity {
 
   private static final int LOGIN_ACTIVITY_REQUEST_CODE = 2234;
   private OkHttpClient httpClient;
+  private String refreshToken;
+
+  @BindView(R.id.activity_login) LinearLayout parent;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -27,10 +36,10 @@ public class LoginActivity extends AppCompatActivity {
     ButterKnife.bind(this);
 
     if (PokemonGo.containsString(PokemonGo.KEY_REFRESH_TOKEN)) {
-      Toast.makeText(LoginActivity.this, "Already logged in.", Toast.LENGTH_SHORT).show();
+      Snackbar.make(parent, "Already logged in.", Snackbar.LENGTH_SHORT).show();
       launchMainActivity();
     } else {
-      Toast.makeText(LoginActivity.this, "Not yet logged in", Toast.LENGTH_SHORT).show();
+      Snackbar.make(parent, "Not yet logged in.", Snackbar.LENGTH_SHORT).show();
     }
   }
 
@@ -62,9 +71,37 @@ public class LoginActivity extends AppCompatActivity {
     if (requestCode == LOGIN_ACTIVITY_REQUEST_CODE) {
       switch (resultCode) {
         case RESULT_OK:
-          Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+          String key = data.getStringExtra("key");
+          try {
+            refreshToken = new LoginTask().execute(key).get();
+            if (refreshToken != null) {
+              Snackbar.make(parent, "Login successful!", Snackbar.LENGTH_SHORT).show();
+              PokemonGo.putString(PokemonGo.KEY_REFRESH_TOKEN, refreshToken);
+            }
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          } catch (ExecutionException e) {
+            e.printStackTrace();
+          }
           break;
       }
+    }
+  }
+
+  class LoginTask extends AsyncTask<String, Void, String> {
+    @Override protected String doInBackground(String... strings) {
+      try {
+        PokemonGo.provider.login(strings[0]);
+        PokemonGo.go = new com.pokegoapi.api.PokemonGo(httpClient);
+        PokemonGo.go.login(PokemonGo.provider);
+
+        return PokemonGo.provider.getRefreshToken();
+      } catch (LoginFailedException e) {
+        e.printStackTrace();
+      } catch (RemoteServerException e) {
+        e.printStackTrace();
+      }
+      return null;
     }
   }
 }
